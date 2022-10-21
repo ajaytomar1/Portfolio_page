142190831ad9b1f74440aa97cd30ff19d6e9df6d
Ajay Tomar <ajaytomar4321@gmail.com>
2022-10-21 13:20:15 +0530

    final commit5

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -19,13 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm^u(m9c3jjild-q-i6c0)$4!30fa!xb&6=l5xex2c!35y2q%*q'
+SECRET_KEY = os.envron['secret_key']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["tomarajay1.herokuapp.com", "localhost"]
 
 # Application definition
 
