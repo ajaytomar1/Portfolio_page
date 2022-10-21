8a7440c570c9838d8af4570ebff29d2e34a2fc52
Ajay Tomar <ajaytomar4321@gmail.com>
2022-10-21 11:08:33 +0530

    initial commit1

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -18,11 +18,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['secret_key'] 
+# SECURITY WARNING: keep the secret key used in production 
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
